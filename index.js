4e7160ecbc9b4ea3457c46718caced2c18ce7b41
Milibeth <124629620+Milibeth@users.noreply.github.com>
2024-09-05 01:06:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let anoAtual = 2024;
+const nomeCompleto = "Milibeth Garcia";
+let idade = 17;
+const cursoFaculdade = "Ciência da Computação";
+let cidadeAtual = "São Jośe dos Pinhais"
+
+console.log(`Olá eu sou a ${nomeCompleto}, eu tenho ${idade} anos, eu moro em ${cidadeAtual} e quero ano que vem estar cursando ${cursoFaculdade}.`);
+
+ anoAtual = 2025;
+
+idade = 18;
+cidadeAtual = "Barcelona"
+
+console.log(`Olá eu sou a ${nomeCompleto}, eu tenho ${idade} anos, eu moro em ${cidadeAtual} e estou cursando  ${cursoFaculdade} na Universitat Barcelona.`);
+
+
+console.log(anoAtual);
