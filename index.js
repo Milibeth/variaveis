436836f03b7f2ace7eaff9c7843ead15d263422d
Milibeth <124629620+Milibeth@users.noreply.github.com>
2024-09-26 01:32:28 +0000

    25/09

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync')
+const entrada = prompt()
+
 let anoAtual = 2024;
 const nomeCompleto = "Milibeth Garcia";
 let idade = 17;
@@ -15,3 +18,31 @@ console.log(`Olá eu sou a ${nomeCompleto}, eu tenho ${idade} anos, eu moro em $
 
 
 console.log(anoAtual);
+
+//Agência de Viagens
+
+var nomeComprador = entrada("Qual é seu nome?");
+var cpfComprador = entrada("Qual é seu cpf?");
+var idadeComprador = entrada ("Qual sua idade?")
+const listaDeDestinos = new Array(
+  " Barcelona", 
+  " Roma",
+  " Paris",
+  " Cancun",
+  " Florida"  
+);
+
+console.log(`Olá, senhora ${nomeComprador}, portadora do cpf ${cpfComprador}, informados que sua passagem para ${listaDeDestinos}, está disponível para compra.`);
+
+console.log(listaDeDestinos[3]);
+console.log("");
+console.log(`Olá senhor ${nomeComprador} adicionamos mais uma viagem a sua lista de interesses.`);
+
+listaDeDestinos.push("Roma");
+
+if(idadeComprador<18){
+  console.log(`Infelizmente não podemos lhe vender uma passagem,pois o senhor é menor de idade`)
+}
+if(idadeComprador>18){
+  console.log(`Parabéns, sua compra foi foi confirmada!, por favor verificar a passagem para ${listaDeDestinos}.`)
+}
